0925c88308cc9ffd60e3c5608bafc5d83130d3e6
mauricio <mauricio.sayao@gmail.com>
2020-10-14 13:53:14 -0400

    Configurando data source

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
+enum Section {
+    case main
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.collectionViewLayout = configureLayout()
+        configDataSource()
     }
     
     func configureLayout() -> UICollectionViewCompositionalLayout {
@@ -28,6 +34,24 @@ class ViewController: UIViewController {
         let section = NSCollectionLayoutSection(group: group)
         
         return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    func configDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: self.collectionView) { (collectionView, indexPath, number) -> UICollectionViewCell? in
+            
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else {
+                fatalError("Cannot create new cell")
+            }
+            
+            cell.textLabel.text = number.description
+            return cell
+        }
+        
+        var initialSnapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        initialSnapshot.appendSections([.main])
+        initialSnapshot.appendItems(Array(1...100))
+        
+        dataSource.apply(initialSnapshot, animatingDifferences: false)
     }
 }
 
